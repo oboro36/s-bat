1e008ca57471848fdb92b71358f3c69494450bac
Oboro <napatarasakorn.c@gmail.com>
2020-01-17 14:15:41 +0900

    hide button

--- components/video/videosearch.js
+++ components/video/videosearch.js
@@ -606,12 +606,12 @@ class VideoSearch extends React.Component {
                                     <Col>
                                         <Button type="primary" icon="search" disabled={this.state.validated.choice1 && this.state.validated.choice2 ? false : true} onClick={this.handleSubmit}>Search</Button>
                                     </Col>
-                                    <Col>
+                                    {/* <Col>
                                         <Button type="danger" onClick={this.clear}>Clear</Button>
                                     </Col>
                                     <Col>
                                         <Button type="dashed" onClick={this.test}>Test</Button>
-                                    </Col>
+                                    </Col> */}
                                 </Row>
                             </Panel>
                         </Collapse>
